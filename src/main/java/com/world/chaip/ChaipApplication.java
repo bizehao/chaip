5c7f5bed05db150038c18e91bd64ba98941581c2
libaigood <1715837891@qq.com>
2019-01-18 13:37:00 +0800

    测试版

--- src/main/java/com/world/chaip/ChaipApplication.java
+++ src/main/java/com/world/chaip/ChaipApplication.java
@@ -3,15 +3,24 @@ package com.world.chaip;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 /**
  * 入口程序
  *
  */
 @MapperScan("com.world.chaip.mapper")
 @SpringBootApplication
-public class ChaipApplication {
+public class ChaipApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChaipApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(ChaipApplication.class);
 	}
 }
